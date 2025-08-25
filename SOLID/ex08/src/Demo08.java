66684a5776ae99f4c05cb1a22a38157034d89f0d
KeshavMakkad <keshavmakkad10@gmail.com>
2025-08-25 09:36:17 +0530

    ex08 done

--- SOLID/ex08/src/Demo08.java
+++ SOLID/ex08/src/Demo08.java
@@ -1,6 +1,6 @@
 public class Demo08 {
     public static void main(String[] args) {
-        Vehicle v = new Bicycle();
-        v.startEngine(); // crash
+        IPedalVehicle v = new Bicycle();
+        v.pedal(5);
     }
 }
